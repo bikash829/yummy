ec90e09988702552717c266273e8166a4e102922
Bikash Chowdhury <chowdhurybikash38@gmail.com>
2023-12-21 15:09:19 +0600

    o Admin can change title section o Admin can add/remove subsection (max 3)

--- section_content/admin.py
+++ section_content/admin.py
@@ -19,9 +19,14 @@ from .models import (
     CompanyInformation,
 )
 
-
+"""/
+    # Slider
+/"""
 admin.site.register(Slider)
 
+"""/
+    # About us
+/"""
 # admin.site.register(About)
 @admin.register(About)
 class AboutAdmin(admin.ModelAdmin):
@@ -29,24 +34,40 @@ class AboutAdmin(admin.ModelAdmin):
 
     def get_section_name(self, obj):
         return obj.section.section_name
-
     # get_section_name.short_description = 'Section Name'
 
+"""/
+    # About us list
+/"""
 admin.site.register(AboutList)
 
-
+"""/
+    # Why us
+/"""
 @admin.register(WhyUs)
 class WhyUsAdmin(admin.ModelAdmin):
     # inlines = [AboutInline]
+    list_display = ('get_section_name','title','icon',)
+
+    def get_section_name(self, obj):
+        return obj.section.section_name
+
     verbose_name = "Why Us"
+    max_whyus_records = 3  # Set the maximum number of records
     
     # This will help you to disbale add functionality
-    def has_add_permission(self, request):
-        return False
+    # def has_add_permission(self, request):
+    #     return False
 
-    # This will help you to disable delete functionaliyt
-    def has_delete_permission(self, request, obj=None):
-        return False
+    # # This will help you to disable delete functionaliyt
+    # def has_delete_permission(self, request, obj=None):
+    #     return False
+
+    def has_add_permission(self, request):
+    # Check if the number of existing records is less than the maximum allowed
+        if WhyUs.objects.count() >= self.max_whyus_records:
+            return False
+        return super().has_add_permission(request)
 
 admin.site.register(Statistics)
 admin.site.register(MenuCategory)
